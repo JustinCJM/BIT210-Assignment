c5a2bb9b1aac1f18ddc7fbfb5b86461e6513878b
Kylow797 <87619255+Kylow797@users.noreply.github.com>
2023-11-12 22:42:58 +0800

    ugly but done

---
+++ Kproduct.php
@@ -0,0 +1,120 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<?php require_once 'includes/config_session.inc.php';
+require_once 'includes/dbh.inc.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Product Catalog</title>
+    <link rel="icon" type="image/x-icon" href="assets/logo.png" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+    />
+    <link
+      rel="stylesheet"
+      href="style.css"
+    />
+         
+</head>
+
+  <body class="vh-190 page-body">
+  <?php
+    $userType = $_SESSION["user_type"] ?? null;  
+    if ($userType === 'merchant') {
+        include 'includes/headers/header_merchant.inc.php';
+    } elseif ($userType === 'customer') {
+        include 'includes/headers/header_customer.inc.php';
+    } elseif ($userType === 'tourism_ministry_officer') {
+        include 'includes/headers/header_officer.inc.php';
+    } else {
+        include 'includes/headers/defaultheader.inc.php';
+    }
+    $productID = $_GET['productid'];
+    $sql = "SELECT product.productName, product.productPrice, product_images.image_path, product.category, product.prodDescription
+            FROM product 
+            LEFT JOIN product_images ON product.productID = product_images.productID
+            WHERE product.productID = $productID";
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    ?>  
+	<div class="container mt-5 mb-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="images p-3">
+                            <div class="text-center p-4"> <img id="main-image" src="<?php echo $row['image_path']?>" width="250" /> </div>
+                            <div class="thumbnail text-center"> <img onclick="change_image(this)" src="<?php echo $row['image_path']?>" width="70"> <img onclick="change_image(this)" src="<?php echo $row['image_path']?>" width="70"> </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="product p-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <a href="Kproductcatalog.php">Back</a>
+                            </div>
+                            <div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand"><?php echo $row['category']?></span>
+                                <h5 class="text-uppercase"><?php echo $row['productName']?></h5>
+                                <div class="price d-flex flex-row align-items-center"> <span class="act-price">RM<?php echo $row['productPrice']?></span>
+                                </div>
+                            </div>
+                            <p class="about"><?php echo $row['prodDescription']?></p>
+                            <div class="sizes mt-5">
+                                
+                            </div>
+                            <div class="cart mt-4 align-items-center"> <button onclick="window.location.href='Kpayment.php?productid=<?php echo urlencode($_GET['productid']); ?>'" class="btn btn-danger text-uppercase mr-2 px-4">Checkout</button>
+                            <i class="fa fa-heart text-muted"></i> <i class="fa fa-share-alt text-muted"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script>    
+function change_image(image){
+
+var container = document.getElementById("main-image");
+
+container.src = image.src;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+
+
+
+
+});</script>
+</html>
