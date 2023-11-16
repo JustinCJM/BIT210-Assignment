ea17cb3815271de2763ea01d8ef80b3f5499d776
Ayayron02 <aaronnabil02@gmail.com>
2023-11-16 13:06:17 +0800

    Update average and total rating

--- includes/reviewForm/reviewForm_contr.inc.php
+++ includes/reviewForm/reviewForm_contr.inc.php
@@ -11,4 +11,6 @@ function is_comment_empty(string $reviewComment) {
 }
 function addReview(object $mysqli, string $comments, int $rating, int $orderID, int $productID){
     setReview($mysqli, $comments, $rating, $orderID, $productID);
+    updateRating($mysqli, $productID);
+    updateAvgRating($mysqli, $productID);
 }
